921640116f3368d82ea16d412b065bb256379f18
Wesley <wesley_souza1996@hotmail.com>
2022-03-09 08:44:48 -0300

    Edit task function

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,44 @@ import Modal from './components/Modal';
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
 
   const deleteTask = (id: number) =>{
     setTaskList(taskList.filter(task=>{return task.id !== id}))
   }
 
+  const hideOrShowModal = (display:boolean) => {
+    const modal = document.querySelector('#modal');
+    modal!.classList.add("hide")
+    if(display){
+      modal!.classList.remove("hide")
+    }
+  }
+  const editTask = (task: ITask): void =>{
+    hideOrShowModal(true);
+    setTaskToUpdate(task)
+  }
+  const updateTask = (id: number, title: string, difficulty: number) =>{
+      const updatedTask: ITask = {id,title,difficulty}
+      const updatedItens = taskList.map((task)=>{
+        return task.id === updatedTask.id ? updatedTask : task
+      })
+
+      setTaskList(updatedItens)
+      hideOrShowModal(false)
+  }
   return (
     <div>
-      <Modal children={<TaskForm btnText='Editar Tarefa' taskList={taskList}/>}/>
+      <Modal children={<TaskForm btnText='Editar Tarefa' taskList={taskList} task={taskToUpdate} handleUpdate={updateTask}/>}/>
       <Header/>
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
+          <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}  />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList ={taskList} handleDelete={deleteTask}/>
+          <TaskList taskList ={taskList} handleDelete={deleteTask} handleEdit = {editTask}/>
         </div>
       </main>
       <Footer/>
